5285a4850b22c3ee0725ca7337adee7ed4795b77
RabeeDmenam2020 <Rabeedmenam@outlook.com>
2021-12-06 11:09:12 +0400

    task one

---
+++ NIT.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NIT</title>
+</head>
+            <?php
+             $unit = 150 ;
+
+             $bill = 0 ;
+
+             if ($unit  <= 50 )
+             {
+                 echo "Total Bill ".$bill = $unit *3.5;
+
+             }elseif($unit  <= 100)
+             {
+                 echo "Total Bill ".$bill = $unit * 4;
+
+             }elseif($unit  <= 150)
+
+             {
+                 echo "Total Bill " .$bill = $unit * 6;
+             }
+
+            ?>
+</body>
+</html>
