a08bd9a61a09b56960a41aceb58c9533e57001b8
Rabeedmenam <Rabeedmenam@outlook.com>
2021-12-12 10:43:11 +0400

    task five

---
+++ opensaveddata.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>saved data </title>
+</head>
+
+<body>
+    <?php
+
+    $mydata = fopen("saved-data.txt", "r");
+    while (!feof($mydata)) {
+        // is_array( $mydata );  
+        echo fgets($mydata) . "<br />";
+    }
+    fclose($mydata);
+
+
+
+    ?>
+    <div class="container">
+        <div class="row">
+            <section>
+                <h1>List Of products </h1>
+                <table class="table-bordered text-center">
+                    <tr>
+                        <th>products_id</th>
+                        <th>products_name</th>
+                        <th> products_quantity</th>
+                        <th>products_model</th>
+                        <th>products_image</th>
+                        <th>products_date_added </th>
+                        <th>products_liked</th>
+                        <th>products_description</th>
+                    </tr>
+                    <?php
+                    // if (is_array($mydata) || is_object($mydata)) {
+               
+                    foreach ($mydata['data'] as $key =>  $value) {
+
+                    ?>
+                        <tr>
+                            <td><?php echo $value['products_id']; ?></td>
+                            <td><?php echo $value['products_name']; ?></td>
+                            <td><?php echo $value['products_quantity']; ?></td>
+                            <td><?php echo $value['products_model']; ?></td>
+                            <td><?php echo $value['products_image']; ?></td>
+                            <td><?php echo $value['products_date_added']; ?></td>
+                            <td><?php echo $value['products_liked']; ?></td>
+                            <!-- <td class="overflow-hidden"><? php  //echo $value['products_description'];
+                                                                ?></td>  -->
+                        </tr>
+
+                    <?php
+                        // }
+                    }
+
+                    ?>
+                </table>
+            </section>
+
+        </div>
+
+
+
+    </div>
+
+
+</body>
+
+</html>
